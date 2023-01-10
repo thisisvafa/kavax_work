8733520840af5bc429cad6df958ddbef611d521f
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-01-10 12:45:19 +0330

    fix paginate style

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -1,14 +1,55 @@
+<style>
+    .pagination-default ul {
+        list-style: none;
+        display: flex;
+        justify-content: center;
+    }
+    .pagination-default ul li {
+        margin: 0 5px;
+    }
+    .pagination-default ul li span {
+        display: inline-block;
+        color: #f1a927;
+        font-size: 15px;
+        font-weight: 500;
+        text-transform: uppercase;
+        letter-spacing: 2.88px;
+        border: 2px solid #f1a927;
+        padding: 10px 20px;
+    }
+    .pagination-default ul .active-page span {
+        display: inline-block;
+        color: #202020;
+        font-size: 15px;
+        font-weight: 500;
+        text-transform: uppercase;
+        letter-spacing: 2.88px;
+        border: 2px solid #f1a927;
+        background-color: #f1a927;
+        padding: 10px 20px;
+    }
+    .pagination-default ul li a {
+        display: inline-block;
+        color: #f1a927;
+        font-size: 15px;
+        font-weight: 500;
+        text-transform: uppercase;
+        letter-spacing: 2.88px;
+        border: 2px solid #f1a927;
+        padding: 10px 20px;
+    }
+</style>
 @if ($paginator->hasPages())
     <nav class="pagination-default">
         <ul>
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
                 <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                    <span aria-hidden="true" class="page-arrow icon-arrow-right-linear"></span>
+                    <span aria-hidden="true" class="page-arrow icon-arrow-right-linear">previous</span>
                 </li>
             @else
                 <li>
-                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')" class="page-arrow icon-arrow-right-linear"></a>
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')" class="page-arrow icon-arrow-right-linear">previous</a>
                 </li>
             @endif
 
@@ -34,11 +75,11 @@
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
                 <li>
-                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')" class="page-arrow icon-arrow-left-linear"></a>
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')" class="page-arrow icon-arrow-left-linear">next</a>
                 </li>
             @else
                 <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span aria-hidden="true" class="page-arrow icon-arrow-left-linear"></span>
+                    <span aria-hidden="true" class="page-arrow icon-arrow-left-linear">next</span>
                 </li>
             @endif
         </ul>
