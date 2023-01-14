43cd220efb3307ffe5f26d781219b6fbd7c535e4
Vafa Pourtavakkoli <vafa_v17@yahoo.com>
2023-01-14 12:32:38 +0330

    fix 404 & optimize images

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,28 +1,14 @@
 @extends('layouts.site.master')
 
 @section('content')
-    <section class="intro-section">
-        <div class="section-bg">
-            <img class="shape" src="{{ asset('assets/site/images/base') }}/heading-bg-shape.png" alt="A young diverse team of digital experts">
-            <img class="bg-img" src="{{ asset('assets/site/images/base') }}/header-image/services-bg.png" alt="A young diverse team of digital experts">
-        </div>
-
-        <div class="content-section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-7 col-sm-8 col-12">
-                        <div class="breadcrumb-block">
-                            <ul>
-                                <li><a href="{{ url('/') }}">Go Home Page</a></li>
-                            </ul>
-                        </div>
-                        <div class="title-heading">
-                            <h1>404</h1>
-                        </div>
-                        <div class="section-text">PAGE NOT FOUND!</div>
-                    </div>
-                </div>
-            </div>
+    <style>
+        .section-bg-err img {
+            width: 100%;
+        }
+    </style>
+    <section class="">
+        <div class="section-bg-err">
+            <img class="shape" src="{{ asset('assets/site/images/base/404.jpg') }}" alt="404">
         </div>
     </section>
 @endsection
