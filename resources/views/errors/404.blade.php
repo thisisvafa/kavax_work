c2957c912f2b050c90518254de380df8638bad49
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-01-17 00:13:21 +0330

    fix 404 responsive

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,10 +5,16 @@
         .section-bg-err img {
             width: 100%;
         }
+        @media (max-width:768px) {
+            .section-bg-err img {
+                height: 100vh;
+                object-fit: cover;
+            }
+        }
     </style>
     <section class="">
         <div class="section-bg-err">
-            <img class="shape" src="{{ asset('assets/site/images/base/404.jpg') }}" alt="404">
+            <img class="shape" src="{{ asset('assets/site/images/base/404-1.jpg') }}" alt="404">
         </div>
     </section>
 @endsection
